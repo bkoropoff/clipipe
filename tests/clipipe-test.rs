7f40c4710862c07a96bb3df1bd91056f0bb5f1a0
Brian Koropoff <bkoropoff@gmail.com>
2025-08-24 20:51:13 -0700

    Add integration test

---
+++ tests/clipipe-test.rs
@@ -0,0 +1,123 @@
+use serde_json::{json, Value};
+use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::path::PathBuf;
+use std::process::{Child, Command, Stdio};
+use std::str::FromStr;
+
+fn clipipe_bin() -> PathBuf {
+    let test_bin = std::env::current_exe().expect("Couldn't find test binary path");
+    let test_dir = test_bin
+        .parent()
+        .unwrap_or_else(|| panic!("Couldn't find test bin directory"));
+    let profile_dir = test_dir
+        .parent()
+        .unwrap_or_else(|| panic!("Couldn't find profile directory"));
+    profile_dir.join(if cfg!(target_os = "windows") {
+        "clipipe.exe"
+    } else {
+        "clipipe"
+    })
+}
+
+struct Clipipe<I, O> {
+    child: Child,
+    input: I,
+    output: O,
+}
+
+impl<I, O> Drop for Clipipe<I, O> {
+    fn drop(&mut self) {
+        self.child.kill().expect("Couldn't terminate clipipe")
+    }
+}
+
+impl<I: BufRead, O: Write> Clipipe<I, O> {
+    fn request(&mut self, message: Value) -> Value {
+        writeln!(self.output, "{}", message).expect("Couldn't write to output");
+        self.output.flush().expect("Couldn't flush output");
+        let mut buffer = String::new();
+        self.input
+            .read_line(&mut buffer)
+            .expect("Couldn't read input");
+        Value::from_str(&buffer).expect("Invalid JSON")
+    }
+}
+
+enum DisplayServer {
+    #[cfg(target_os = "linux")]
+    Wayland,
+    #[cfg(target_os = "linux")]
+    X11,
+    #[cfg(target_os = "windows")]
+    Windows,
+}
+
+const TEST_DATA: &str = "corndog";
+
+fn spawn(server: DisplayServer) -> Clipipe<impl BufRead, impl Write> {
+    let mut cmd = Command::new(clipipe_bin());
+    cmd.stdout(Stdio::piped()).stdin(Stdio::piped());
+    match server {
+        #[cfg(target_os = "linux")]
+        DisplayServer::Wayland => {
+            cmd.env_remove("DISPLAY");
+        }
+        #[cfg(target_os = "linux")]
+        DisplayServer::X11 => {
+            cmd.env_remove("WAYLAND_DISPLAY");
+        }
+        #[cfg(target_os = "windows")]
+        DisplayServer::Windows => (),
+    };
+
+    let mut child = cmd.spawn().expect("Couldn't run clipipe");
+    let input = BufReader::new(child.stdout.take().unwrap());
+    let output = BufWriter::new(child.stdin.take().unwrap());
+    Clipipe {
+        child,
+        input,
+        output,
+    }
+}
+
+mod tests {
+    use super::*;
+    use rstest::rstest;
+    use rstest_reuse::{self, *};
+
+    #[cfg(target_os = "linux")]
+    mod template {
+        use super::*;
+        #[template]
+        #[rstest]
+        #[case::wayland(DisplayServer::Wayland)]
+        #[case::x11(DisplayServer::X11)]
+        fn display(#[case] _server: DisplayServer) {}
+    }
+
+    #[cfg(target_os = "windows")]
+    mod template {
+        use super::*;
+        #[template]
+        #[rstest]
+        #[case::windows(DisplayServer::Windows)]
+        fn display(#[case] _server: DisplayServer) {}
+    }
+
+    #[apply(template::display)]
+    fn copy_paste(#[case] server: DisplayServer) {
+        let mut clipipe = spawn(server);
+        assert_eq!(
+            clipipe.request(json!({"action": "copy", "data": TEST_DATA})),
+            json!({"success": true})
+        );
+
+        let response = clipipe.request(json!({"action": "paste"}));
+        println!("{}", response);
+        assert_eq!(response["success"], Value::Bool(true));
+        assert_eq!(response["data"], Value::String(TEST_DATA.into()));
+        if let Some(mime) = response.get("mime") {
+            assert_eq!(mime, "text/plain")
+        }
+    }
+}
